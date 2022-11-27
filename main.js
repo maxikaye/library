bb0e3e01300344a40e1776e1911820ed54315eb6
maxikaye <mr.max.kaye@gmail.com>
2022-11-27 14:10:10 -0500

    Add sample library and docstring for constructor

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const newBook = document.querySelector('#btn-new-book');
 
-let myLibrary = [
-    
-];
+let myLibrary = [];
 
+/**
+ * Book Constructor
+ * @param {string}} title 
+ * @param {string} author 
+ * @param {number (integer)} pages 
+ * @param {boolean} read 
+ */
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -20,5 +25,11 @@ function displayLibrary() {
 }
 
 // new book button
-newBook.addEventListener('click', addBookToLibrary, false);
-//
+newBook.addEventListener('click', _, false);
+
+
+// Sample Library
+const bhagavadGita = new Book('Bhagavad Gita', 'Krishna Dvaipayana', 101, true);
+const newTestament = new Book('The New Testament from Peshitta Aramaic', 'Georgae M Lamsa', 412, true);
+const holyQuran = new Book('The Holy Quaran', 'Mohammed', 292, false);
+myLibrary.push(bhagavadGita, newTestament, holyQuran);
