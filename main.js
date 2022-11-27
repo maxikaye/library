bb68217357a9ce9c7cd1cb99b4e8c49fc4b04a94
maxikaye <mr.max.kaye@gmail.com>
2022-11-27 10:38:51 -0500

    Add Book constructor

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
+const newBook = document.querySelector('#btn-new-book');
+
 let myLibrary = [
     
 ];
 
-function Book() {
-
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(e) {
+  e.preventDefault();
 }
+
+function displayLibrary() {
+    // loop thru myLibrary
+}
+
+// new book button
+newBook.addEventListener('click', addBookToLibrary, false);
+//
